5768815fd9bf0a6c8e39ab9eb1a1db7750a32b77
jcmaker0627 <jcmaker0627@gmail.com>
2022-05-20 13:55:21 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ import Admin from "./screens/Admin";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route exact path="/navy-admin8585" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
